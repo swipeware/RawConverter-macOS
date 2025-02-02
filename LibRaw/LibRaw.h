d4a6402a8025562e0c86ec0ed1385760fd5a1d64
Ingemar Bergmark <ingemar@swipeware.com>
2025-02-02 22:46:27 +0900

    Initial Commit

---
+++ LibRaw/LibRaw.h
@@ -0,0 +1,18 @@
+//
+//  LibRaw.h
+//  LibRaw
+//
+//  Created by Ingemar Bergmark on 2025-02-02.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LibRaw.
+FOUNDATION_EXPORT double LibRawVersionNumber;
+
+//! Project version string for LibRaw.
+FOUNDATION_EXPORT const unsigned char LibRawVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LibRaw/PublicHeader.h>
+
+
