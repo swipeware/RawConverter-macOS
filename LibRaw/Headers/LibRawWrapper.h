d46b43880cd0fc8677f8bc537ffa60a1e2f460ce
Ingemar Bergmark <ingemar@swipeware.com>
2025-02-03 00:01:27 +0900

    Checkpoint

--- LibRaw/Headers/LibRawWrapper.h
+++ LibRaw/Headers/LibRawWrapper.h
@@ -8,5 +8,22 @@
 #ifndef LibRawWrapper_h
 #define LibRawWrapper_h
 
+#import <Foundation/Foundation.h>
+//#import "libraw.h"
+
+@interface LibRawWrapper : NSObject
+
+///// The pointer to the libraw data structure.
+//@property (nonatomic, assign, readonly) libraw_data_t *rawData;
+
+/**
+ Initializes the wrapper with the specified flags.
+
+ @param flags An unsigned integer representing initialization flags.
+ @return An instance of LibRawWrapper if initialization succeeds; otherwise, nil.
+ */
+- (instancetype)initWithFlags:(unsigned int)flags;
+
+@end
 
 #endif /* LibRawWrapper_h */
--- LibRaw/LibRawWrapper.m
+++ LibRaw/LibRawWrapper.m
@@ -5,6 +5,37 @@
 //  Created by Ingemar Bergmark on 2025-02-02.
 //
 
-#import <Foundation/Foundation.h>
 #import "LibRawWrapper.h"
 #import "libraw.h"
+
+@implementation LibRawWrapper
+
+- (instancetype)initWithFlags:(unsigned int)flags {
+  self = [super init];
+//  if (self) {
+//    // Initialize libraw using the provided flags.
+//    _rawData = libraw_init(flags);
+//    
+//    if (!_rawData) {
+//      NSLog(@"Error: libraw_init failed with flags: %u", flags);
+//      // Initialization failed, so return nil.
+//      return nil;
+//    }
+//    
+//    NSLog(@"Successfully initialized libraw with flags: %u", flags);
+//  }
+  return self;
+}
+
+- (void)dealloc {
+//  // Clean up the libraw data structure if it was successfully created.
+//  if (_rawData) {
+//    libraw_close(_rawData);
+//  }
+  
+#if !__has_feature(objc_arc)
+  [super dealloc];
+#endif
+}
+
+@end
