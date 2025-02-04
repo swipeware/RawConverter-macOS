92c2119e2f87b99a36b2abcb5cb68cdf47029394
Ingemar Bergmark <ingemar@swipeware.com>
2025-02-04 22:17:43 +0900

    Checkpoint (libraw built with Apple's build tools)

--- LibRaw/Headers/LibRawWrapper.h
+++ LibRaw/Headers/LibRawWrapper.h
@@ -15,9 +15,20 @@ NS_ASSUME_NONNULL_BEGIN
 // Public interface
 @interface LibRawWrapper : NSObject
 
+// Initializers
 - (instancetype)init NS_UNAVAILABLE; // Not needed as initWithError is the main initializer
 - (nullable instancetype)initWithError:(NSError **)error NS_DESIGNATED_INITIALIZER NS_SWIFT_NAME(init()) ;
 
+// Class methods
++ (NSString *)libRawVersion;
++ (int32_t)cameraCount;
++ (NSArray<NSString *> *)supportedCameras;
+  
+// Instance methods
+- (BOOL)convertRawToTiffWithRawFilePath:(NSString *)rawFilePath
+                         tiffOutputPath:(NSString *)tiffOutputPath
+                               settings:(NSArray<NSString *> *)settings
+                                  error:(NSError **)errorHandler;
 @end
 
 NS_ASSUME_NONNULL_END
